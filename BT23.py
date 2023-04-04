8830d9f9802b252494a10de31020f00ec62fee57
mymy19 <129322146+mymy19@users.noreply.github.com>
2023-04-04 09:11:50 +0700

    Add files via upload

--- BT23.py
+++ BT23.py
@@ -1,8 +1,9 @@
 a=input('Nhap hai canh ke cua tam giac vuong:')
 ke1=input('')
 ke2=input('')
-
 import math
 huyen=math.sqrt(float(ke1)**2 + float(ke2)**2)
 
-print('Canh ke thu nhat la: ' + str(ke1) + ', canh ke thu hai: ' + str(ke2) + ', co canh huyen =', round(huyen,2))
+print('Canh ke thu nhat la:', ke1,end=",")
+print(' canh ke thu hai:',ke2, end=",")
+print(' co canh huyen =', round(huyen,2))
---
+++ BT25.py
@@ -0,0 +1,7 @@
+n=input('So tien ban dau: ')
+k=input('So thang gui: ')
+T=input('Lai suat/ thang: ')
+
+m=float(n)*(1+float(k)*float(T))
+print('Voi so tien ban dau '+ str(n)+ ', sau '+ str(k)+' thang gui, lai suat '+ str(T)+'/ thang')
+print('Thi so tien nhan duoc cuoi ky la:', m)
